0d81cd1cee71584a02d120b95e304fb59b620352
tsukhu <tsukhu@hcl.com>
2016-04-06 16:55:36 +0530

    commit all

--- app/navbar.component.ts
+++ app/navbar.component.ts
@@ -1,7 +1,7 @@
 import {Component} from 'angular2/core';
-import {RouteConfig,ROUTER_DIRECTIVES,Router} from 'angular2/router';
+import {RouteConfig, ROUTER_DIRECTIVES, Router} from 'angular2/router';
 
-declare var jQuery:any;
+declare var jQuery: any;
 
 @Component({
     selector: 'navbar',
@@ -11,14 +11,14 @@ declare var jQuery:any;
 export class NavBarComponent {
 
    	constructor(private _router: Router) {
-       
-	}
 
-	isCurrentRoute(route) {
-		var instruction = this._router.generate(route);
+    }
+
+    isCurrentRoute(route) {
+        var instruction = this._router.generate(route);
         return this._router.isRouteActive(instruction);
-	}
-    
+    }
+
     // collapse Navbar once clicked
     clicked() {
         jQuery('.navbar-collapse.in').collapse('hide');
